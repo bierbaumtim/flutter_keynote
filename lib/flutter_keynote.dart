6c75752505f963174b7a0e77877888734e71535d
tb2761 <tb2706@outlook.de>
2020-02-14 22:23:06 +0100

    added missing export for KeynoteProvider

--- lib/flutter_keynote.dart
+++ lib/flutter_keynote.dart
@@ -2,3 +2,4 @@ library flutter_keynote;
 
 export 'package:flutter_keynote/src/keynote_app.dart';
 export 'package:flutter_keynote/src/keynote_transition.dart';
+export 'package:flutter_keynote/src/providers/keynote_provider.dart';
